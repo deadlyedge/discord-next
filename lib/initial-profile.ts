430afdf6656aa7d6c40769d01bdb36c385945dc7
xdream oldlu <xdream@gmail.com>
2023-11-15 11:08:32 +0800

    done initial modal

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -7,23 +7,23 @@ export const initialProfile =async () => {
 
   if (!user) {
     redirectToSignIn();
-  }
+  } else {
 
-  const profile = await db.profile.findUnique({
-    where: { userId: user.id }
-  })
+    const profile = await db.profile.findUnique({
+      where: { userId: user.id }
+    })
 
-  if (profile) {
-    return profile
-  }
+    if (profile) {
+      return profile
+    }
 
-  const newProfile = await db.profile.create({
-    data: { 
-      userId: user.id ,
-      name: user.firstName + " " + user.lastName,
-      imageUrl: user.imageUrl,
-      email: user.emailAddresses[0].emailAddress
-    }})
+    const newProfile = await db.profile.create({
+      data: { 
+        userId: user.id ,
+        name: user.firstName + " " + user.lastName,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress
+      }})
     
-  return newProfile
+  return newProfile}
 }
