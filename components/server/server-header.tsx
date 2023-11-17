0d5804fe5252a00a8ef46fae46be372dcc901c02
xdream oldlu <xdream@gmail.com>
2023-11-17 10:56:15 +0800

    done server dropdown menu

---
+++ components/server/server-header.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { ServerWithMembersWithProfiles } from "@/types"
+import { MemberRole } from "@prisma/client"
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus,
+  Users,
+} from "lucide-react"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+type ServerHeaderProps = {
+  server: ServerWithMembersWithProfiles
+  role?: MemberRole
+}
+
+export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const isAdmin = role === MemberRole.ADMIN
+  const isModerator = isAdmin || role === MemberRole.MODERATOR
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className='focus:outline-none' asChild>
+        <button className='w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition'>
+          {server.name}
+          <ChevronDown className='h-5 w-5 ml-auto' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]'>
+        {isModerator && (
+          <DropdownMenuItem className='px-3 text-indigo-600 dark:text-indigo-400 py-2 text-sm cursor-pointer'>
+            邀请加入
+            <UserPlus className='h-4 w-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer'>
+            服务设置
+            <Settings className='h-4 w-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer'>
+            管理成员
+            <Users className='h-4 w-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer'>
+            建立频道
+            <PlusCircle className='h-4 w-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuSeparator />
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer text-rose-500'>
+            删除服务器
+            <Trash className='h-4 w-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer text-rose-500'>
+            离开服务
+            <LogOut className='h-4 w-4 ml-auto' />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
