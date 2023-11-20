89fb6a74a12aeb28f17b6e6fac8e643cee462b5d
xdream oldlu <xdream@gmail.com>
2023-11-20 11:00:18 +0800

    finish 6 hours, good job

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,13 +1,30 @@
 import { redirect } from "next/navigation"
-import { ChannelType } from "@prisma/client"
+import { ChannelType, MemberRole } from "@prisma/client"
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react"
 
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 
 import { ServerHeader } from "./server-header"
+import { ServerSearch } from "./server-search"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 type ServerSidebarProps = {
   serverId: string
+}
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className='mr-2 h-4 w-4' />,
+  [ChannelType.AUDIO]: <Mic className='mr-2 h-4 w-4' />,
+  [ChannelType.VIDEO]: <Video className='mr-2 h-4 w-4' />,
+}
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className='h-4 w-4 mr-2 text-indigo-500' />
+  ),
+  [MemberRole.ADMIN]: <ShieldAlert className='h-4 w-4 mr-2 text-rose-500' />,
 }
 
 export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
@@ -58,6 +75,50 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   return (
     <div className='flex flex-col h-full text-primary w-full dark:bg-[#2b2d31] bg-[#f2f3f5]'>
       <ServerHeader server={server} role={role} />
+      <ScrollArea className='flex-1 px-3'>
+        <div className='mt-2'>
+          <ServerSearch
+            data={[
+              {
+                label: "文字频道",
+                type: "channel",
+                data: textChannels?.map((channel) => ({
+                  icon: iconMap[channel.type],
+                  name: channel.name,
+                  id: channel.id,
+                })),
+              },
+              {
+                label: "声音频道",
+                type: "channel",
+                data: audioChannels?.map((channel) => ({
+                  icon: iconMap[channel.type],
+                  name: channel.name,
+                  id: channel.id,
+                })),
+              },
+              {
+                label: "视频频道",
+                type: "channel",
+                data: videoChannels?.map((channel) => ({
+                  icon: iconMap[channel.type],
+                  name: channel.name,
+                  id: channel.id,
+                })),
+              },
+              {
+                label: "成员",
+                type: "member",
+                data: members?.map((member) => ({
+                  icon: roleIconMap[member.role],
+                  name: member.profile.name,
+                  id: member.id,
+                })),
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   )
 }
