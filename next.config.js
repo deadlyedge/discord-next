6c66872c549c35682a785bd9972c531250836e3f
xdream oldlu <xdream@gmail.com>
2023-11-22 20:40:49 +0800

    remove bufferutil and utf-8-validate

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   webpack: (config) => {
     config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil",
+      // "utf-8-validate": "commonjs utf-8-validate",
+      // bufferutil: "commonjs bufferutil",
     })
 
     return config
