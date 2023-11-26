263a67536f8b4cc69fd95df7dc309c47a9ae3fcc
xdream oldlu <xdream@gmail.com>
2023-11-26 10:52:50 +0800

    just update

--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -3,12 +3,14 @@ import { auth } from "@clerk/nextjs"
 import { db } from "@/lib/db"
 
 export const currentProfile = async () => {
+  // 根据登录状况获取用户ID
   const { userId } = auth()
 
   if (!userId) {
     return null
   }
 
+  // 根据用户ID，从数据库获取用户信息
   const profile = await db.profile.findUnique({
     where: {
       userId,
